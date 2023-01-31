92b2aaeb3a225853f9adae3a188c3a0420f5f7be
md <mehul.java@gmail.com>
2023-01-31 21:20:30 +0530

    Completed the Header component. Made it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 export default function Header() {
+  const [pageState, setPageState] = useState("Sign in");
+
   const location = useLocation();
   const navigate = useNavigate();
-  function pathMathRoute(route) {
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState("Profile");
+      } else {
+        setPageState("Sign in");
+      }
+    });
+  }, [auth]);
+
+  function pathMatchRoute(route) {
     if (route === location.pathname) {
       return true;
     }
   }
 
   const homeLiCss = `cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-    pathMathRoute("/") && "text-black border-b-red-500"
+    pathMatchRoute("/") && "text-black border-b-red-500"
   }`;
   const offersLiCss = `cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-    pathMathRoute("/offers") && "text-black border-b-red-500"
+    pathMatchRoute("/offers") && "text-black border-b-red-500"
   }`;
   const signInLiCss = `cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-    pathMathRoute("/sign-in") && "text-black border-b-red-500"
+    (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) &&
+    "text-black border-b-red-500"
   }`;
   return (
     <div className="bg-white border-b shadow-sm sticky top-0 z-50">
@@ -38,8 +52,8 @@ export default function Header() {
             <li className={offersLiCss} onClick={() => navigate("/offers")}>
               Offers
             </li>
-            <li className={signInLiCss} onClick={() => navigate("/sign-in")}>
-              Sign in
+            <li className={signInLiCss} onClick={() => navigate("/profile")}>
+              {pageState}
             </li>
           </ul>
         </div>
