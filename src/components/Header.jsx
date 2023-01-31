6e416982337badee12d7fef2bc4c0512545f647e
md <mehul.java@gmail.com>
2023-01-31 22:10:20 +0530

    created a Spinner component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,7 +34,7 @@ export default function Header() {
     "text-black border-b-red-500"
   }`;
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
