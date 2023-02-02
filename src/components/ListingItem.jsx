707a320650a1b43a891022b85a699ba6668acff3
md <mehul.java@gmail.com>
2023-02-02 14:07:35 +0530

    add delete and edit functionality to the my listing section

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,26 +1,27 @@
-import React from "react";
 import Moment from "react-moment";
 import { Link } from "react-router-dom";
 import { MdLocationOn } from "react-icons/md";
-export default function ListingItem({ listing, id }) {
+import { FaTrash } from "react-icons/fa";
+import { MdEdit } from "react-icons/md";
+export default function ListingItem({ listing, id, onEdit, onDelete }) {
   return (
-    <li className="bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]">
-      <Link to={`/category/${listing.type}/${id}`} className="contents">
+    <li className="relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]">
+      <Link className="contents" to={`/category/${listing.type}/${id}`}>
         <img
+          className="h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in"
           loading="lazy"
           src={listing.imgUrls[0]}
           alt=""
-          className="h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in"
         />
         <Moment
-          fromNow
           className="absolute top-2 left-2 bg-[#3377cc] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg"
+          fromNow
         >
           {listing.timestamp?.toDate()}
         </Moment>
-        <div className="w-full p-[10px] ">
+        <div className="w-full p-[10px]">
           <div className="flex items-center space-x-1">
-            <MdLocationOn className="h4 w-4 text-green-600" />
+            <MdLocationOn className="h-4 w-4 text-green-600" />
             <p className="font-semibold text-sm mb-[2px] text-gray-600 truncate">
               {listing.address}
             </p>
@@ -53,6 +54,18 @@ export default function ListingItem({ listing, id }) {
           </div>
         </div>
       </Link>
+      {onDelete && (
+        <FaTrash
+          className="absolute bottom-2 right-2 h-[14px] cursor-pointer text-red-500"
+          onClick={() => onDelete(listing.id)}
+        />
+      )}
+      {onEdit && (
+        <MdEdit
+          className="absolute bottom-2 right-7 h-4 cursor-pointer "
+          onClick={() => onEdit(listing.id)}
+        />
+      )}
     </li>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { getAuth, updateProfile } from "firebase/auth";
 import {
   collection,
+  deleteDoc,
   doc,
   getDocs,
   orderBy,
@@ -8,35 +9,32 @@ import {
   updateDoc,
   where,
 } from "firebase/firestore";
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { db } from "../firebase";
 import { FcHome } from "react-icons/fc";
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
 import ListingItem from "../components/ListingItem";
+
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
-
   const [changeDetail, setChangeDetail] = useState(false);
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
-
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
   const { name, email } = formData;
-
   function onLogout() {
     auth.signOut();
     navigate("/");
   }
-
   function onChange(e) {
-    setFormData((formData) => ({
-      ...formData,
+    setFormData((prevState) => ({
+      ...prevState,
       [e.target.id]: e.target.value,
     }));
   }
@@ -47,10 +45,12 @@ export default function Profile() {
         await updateProfile(auth.currentUser, {
           displayName: name,
         });
+
         // update name in the firestore
+
         const docRef = doc(db, "users", auth.currentUser.uid);
         await updateDoc(docRef, {
-          name: name,
+          name,
         });
       }
       toast.success("Profile details updated");
@@ -58,18 +58,17 @@ export default function Profile() {
       toast.error("Could not update the profile details");
     }
   }
-
   useEffect(() => {
     async function fetchUserListings() {
       const listingRef = collection(db, "listings");
       const q = query(
         listingRef,
         where("userRef", "==", auth.currentUser.uid),
-        orderBy("timestamp", "des")
+        orderBy("timestamp", "desc")
       );
-      const queySnap = await getDocs(q);
+      const querySnap = await getDocs(q);
       let listings = [];
-      queySnap.forEach((doc) => {
+      querySnap.forEach((doc) => {
         return listings.push({
           id: doc.id,
           data: doc.data(),
@@ -80,23 +79,40 @@ export default function Profile() {
     }
     fetchUserListings();
   }, [auth.currentUser.uid]);
-
+  async function onDelete(listingID) {
+    if (window.confirm("Are you sure you want to delete?")) {
+      await deleteDoc(doc(db, "listings", listingID));
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingID
+      );
+      setListings(updatedListings);
+      toast.success("Successfully deleted the listing");
+    }
+  }
+  function onEdit(listingID) {
+    navigate(`/edit-listing/${listingID}`);
+  }
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
         <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
         <div className="w-full md:w-[50%] mt-6 px-3">
           <form>
+            {/* Name Input */}
+
             <input
               type="text"
               id="name"
               value={name}
               disabled={!changeDetail}
               onChange={onChange}
-              className={` mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
+              className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
                 changeDetail && "bg-red-200 focus:bg-red-200"
               }`}
             />
+
+            {/* Email Input */}
+
             <input
               type="email"
               id="email"
@@ -104,13 +120,14 @@ export default function Profile() {
               disabled
               className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
             />
+
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
-              <p className="flex items-center">
+              <p className="flex items-center ">
                 Do you want to change your name?
                 <span
                   onClick={() => {
                     changeDetail && onSubmit();
-                    setChangeDetail((cd) => !cd);
+                    setChangeDetail((prevState) => !prevState);
                   }}
                   className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
                 >
@@ -119,7 +136,7 @@ export default function Profile() {
               </p>
               <p
                 onClick={onLogout}
-                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer "
+                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
               >
                 Sign out
               </p>
@@ -127,7 +144,7 @@ export default function Profile() {
           </form>
           <button
             type="submit"
-            className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800 "
+            className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
           >
             <Link
               to="/create-listing"
@@ -145,12 +162,14 @@ export default function Profile() {
             <h2 className="text-2xl text-center font-semibold mb-6">
               My Listings
             </h2>
-            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6">
+            <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
               {listings.map((listing) => (
                 <ListingItem
                   key={listing.id}
-                  listing={listing.data}
                   id={listing.id}
+                  listing={listing.data}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
